13d081e065d40495516bfc183796ab9f6f052435
Srinjoy Dutta <89676505+SrinjoyDutta1@users.noreply.github.com>
2024-11-16 17:41:24 -0500

    Added greet method

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,10 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greet("CodeQL");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
